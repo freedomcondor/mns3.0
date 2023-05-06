0dede9a5263cf1a55b596c41f8a13fbe99e617b5
Weixu Zhu <freedomcondor@126.com>
2023-05-06 21:58:53 +0200

    Improve test21 scalability analyze script

--- src/testing/test_21_scalability/scripts/analyze.in.py
+++ src/testing/test_21_scalability/scripts/analyze.in.py
@@ -95,8 +95,35 @@ def readDataFile(fileName) :
 	return step, mem, time
 
 #----------------------------------------------------------------
+def slide(A,B,C,D,X) :
+	A_res = []
+	B_res = []
+	C_res = []
+	D_res = []
+
+	index_dict = {}
+	total = 0
+	for i in range(0, len(X)) :
+		if str(X[i]) not in index_dict :
+			index_dict[str(X[i])] = total
+			total = total + 1
+			A_res.append([])
+			B_res.append([])
+			C_res.append([])
+			D_res.append([])
+
+		idx = index_dict[str(X[i])]
+		A_res[idx].append(A[i])
+		B_res[idx].append(B[i])
+		C_res[idx].append(C[i])
+		D_res[idx].append(D[i])
+
+	return A_res, B_res, C_res, D_res
+
+#----------------------------------------------------------------
 #dataFolder = "scalability_test"
-dataFolder = "scalability_test_officePC"
+#dataFolder = "scalability_test_officePC"
+dataFolder = "scalability_test_m5zn6xlarge"
 dataFile = "record.dat"
 
 tests = []
@@ -106,7 +133,11 @@ mems = []
 
 testCases = []
 for subFolder in getSubfolders(dataFolder) :
-	subFolderDataFile = dataFolder + "/" + subFolder + "/" + dataFile
+	testCases.append(int(subFolder))
+testCases.sort()
+
+for subFolder in testCases :
+	subFolderDataFile = dataFolder + "/" + str(subFolder) + "/" + dataFile
 	step, mem, time = readDataFile(subFolderDataFile)
 
 	for i in range(0, len(step)) :
@@ -116,14 +147,25 @@ for subFolder in getSubfolders(dataFolder) :
 			times.append(time[i])
 			mems.append(mem[i])
 
+tests_slide_test, steps_slide_test, times_slide_test, mems_slide_test = slide(tests, steps, times, mems, tests)
+tests_slide_step, steps_slide_step, times_slide_step, mems_slide_step = slide(tests, steps, times, mems, steps)
+
 #----------------------------------------------------------------
 fig = plt.figure()
 
 ax1 = fig.add_subplot(1, 2, 1, projection='3d')
 ax2 = fig.add_subplot(1, 2, 2, projection='3d')
 
-ax1.scatter3D(tests, steps, times, c=times, cmap='viridis')
-ax2.scatter3D(tests, steps, mems, c=mems, cmap='viridis')
+#ax1.scatter3D(tests, steps, times, c=times, cmap='viridis')
+#ax2.scatter3D(tests, steps, mems, c=mems, cmap='viridis')
+
+for i in range(0, len(tests_slide_test)) :
+	ax1.plot(tests_slide_test[i], steps_slide_test[i], times_slide_test[i])
+	ax2.plot(tests_slide_test[i], steps_slide_test[i], mems_slide_test[i])
+
+for i in range(0, len(tests_slide_step)) :
+	ax1.plot(tests_slide_step[i], steps_slide_step[i], times_slide_step[i])
+	ax2.plot(tests_slide_step[i], steps_slide_step[i], mems_slide_step[i])
 
 ax1.set_title("time per step")
 ax1.set_xlabel("swarm scale")
