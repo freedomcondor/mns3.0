ee5b02ef37f2b88c1445e02c47aa10a67c49699b
Weixu Zhu <freedomcondor@126.com>
2023-05-10 14:37:06 +0200

    Add replay loop function and python scripts

---
+++ src/extensions/replay_loop_functions.h
@@ -0,0 +1,50 @@
+#ifndef MY_LOOP_FUNCTIONS_H
+#define MY_LOOP_FUNCTIONS_H
+
+namespace argos {
+   class CDebugEntity;
+}
+
+#include <fstream>
+
+#include <argos3/core/simulator/loop_functions.h>
+#include <argos3/plugins/robots/drone/simulator/drone_entity.h>
+#include "pipuck_ext/pipuck_ext_entity.h"
+#include "../qtopengl_extensions/my_qtopengl_user_functions.h"
+
+namespace argos {
+
+   class CReplayLoopFunctions : public CLoopFunctions {
+   public:
+
+      CReplayLoopFunctions() {}
+
+      virtual ~CReplayLoopFunctions() {}
+
+      virtual void Init(TConfigurationNode& t_tree) override;
+
+      virtual void PreStep() override;
+
+   private:
+
+      struct STrackedEntity {
+         STrackedEntity(CEntity* pc_entity,
+                        CEmbodiedEntity* pc_embodied_entity,
+                        CDebugEntity* pc_debug_entity,
+                        std::string str_log_folder) :
+            Entity(pc_entity),
+            EmbodiedEntity(pc_embodied_entity),
+            DebugEntity(pc_debug_entity),
+            LogFile(str_log_folder + "/" + pc_entity->GetId() + ".log") {}
+         CEntity* Entity;
+         CEmbodiedEntity* EmbodiedEntity;
+         CDebugEntity* DebugEntity;
+         std::ifstream LogFile;
+      };
+
+      std::vector<STrackedEntity> m_vecTrackedEntities;
+   };
+}
+
+#endif
+
