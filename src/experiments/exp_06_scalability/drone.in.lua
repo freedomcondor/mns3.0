f7c9af0b9ec76d017d7739e5655ca8320ae1aec2
Weixu Zhu <zhuweixu_harry@126.com>
2023-05-19 17:12:25 +0200

    Developing scalability scenario

--- src/experiments/exp_06_scalability/drone.in.lua
+++ src/experiments/exp_06_scalability/drone.in.lua
@@ -95,6 +95,42 @@ function create_navigation_node(vns)
 		sendChilrenNewState(vns, _newState)
 	end
 
+	local function find_marker(vns, priority, priority_only)
+		-- find marker
+		local marker
+		local marker_behind
+		local dis = math.huge
+		local dis_behind = -math.huge
+
+		local priority_list = {}
+		if priority ~= nil then
+			for i, ob in ipairs(vns.collectivesensor.totalObstaclesList) do
+				if ob.type == priority then
+					table.insert(priority_list, ob)
+				end
+			end
+		end
+		if #priority_list == 0 and priority_only ~= true then
+			priority_list = vns.collectivesensor.totalObstaclesList
+		end
+
+		for i, ob in ipairs(priority_list) do
+				local dirVec = vector3(1,0,0):rotate(ob.orientationQ)
+				local horizontal_shadow = ob.positionV3:dot(dirVec)
+				if horizontal_shadow > 0 and horizontal_shadow < dis then
+					marker = ob
+					dis = horizontal_shadow
+				end
+				if horizontal_shadow <= 0 and horizontal_shadow > dis_behind then
+					marker_behind = ob
+					dis_behind = horizontal_shadow
+				end
+		end
+		if marker == nil then marker = marker_behind end
+		return marker
+	end
+
+
 return function()
 	stateCount = stateCount + 1
 	-- if I receive switch state cmd from parent
@@ -105,21 +141,53 @@ return function()
 	-- fail safe
 	if vns.scalemanager.scale["drone"] == 1 and
 	   vns.api.actuator.flight_preparation.state == "navigation" then
-		vns.Spreader.emergency_after_core(vns, vector3(0,0,0.5), vector3())
+		if vns.brainkeeper ~= nil and vns.brainkeeper.brain ~= nil then
+			local target = vns.brainkeeper.brain.positionV3 + vector3(0,0,5)
+			vns.Spreader.emergency_after_core(vns, target:normalize() * 0.5, vector3())
+		else
+			vns.Spreader.emergency_after_core(vns, vector3(0,0,0.5), vector3())
+		end
 		return false, true
 	end
 
 	-- state
 	-- init
 	if state == "init" then
-		if stateCount == 300 then
-			newState(vns, "forward")
+		if vns.parentR == nil and vns.api.stepCount > 100 then
+			if vns.driver.all_arrive == true then
+				switchAndSendNewState(vns, "forward")
+			end
 		end
 	-- forward
 	elseif state == "forward" then
 		if vns.parentR == nil then
-			local forward_velocity = vector3(0.1, 0, 0)
-			vns.Spreader.emergency_after_core(vns, forward_velocity, vector3())
+			local marker = find_marker(vns)
+			if marker == nil then
+				vns.Spreader.emergency_after_core(vns, vector3(0.1, 0, 0), vector3())
+				return false, true
+			end
+
+			local swarm_size = vns.scalemanager.scale["drone"]
+			local side_length = math.ceil(math.pow(swarm_size, 1.0/3))
+
+			local offset = vector3(-(side_length-1)*0.5 * 1.5, -(side_length-1)*0.5 * 1.5, 1)
+
+			-- vertical speed
+			local target_position = marker.positionV3 + offset:rotate(marker.orientationQ)
+			local dirVec = vector3(1,0,0):rotate(marker.orientationQ)
+			local vertical_position = target_position - target_position:dot(dirVec) * dirVec
+			local vertical_speed = vertical_position * 0.1
+			if vertical_speed:length() > 0.3 then
+				vertical_speed = vertical_speed * (0.3 / vertical_speed:length())
+			end
+
+			-- horizontal speed
+			-- move forward with speed, calculate into move_speed
+			local move_speed = vector3(dirVec):rotate(marker.orientationQ) * 0.3
+
+			-- move forward with vertical speed and move speed
+			vns.setGoal(vns, vector3(0,0,0), marker.orientationQ)
+			vns.Spreader.emergency_after_core(vns, vertical_speed + move_speed, vector3())
 		end
 	end
 
--- src/experiments/exp_06_scalability/run.in.py
+++ src/experiments/exp_06_scalability/run.in.py
@@ -6,26 +6,48 @@ import os
 
 # drone and pipuck
 
-n_drone = Experiment_length   # abuse experiment length for drone numbers
+# abuse experiment length for drone numbers
+if Experiment_length == None :
+    Experiment_length = 27
+n_drone = Experiment_length
+Experiment_length = n_drone * 100
+
 n_side = n_drone ** (1/3)
-half_side_length = n_side * 0.5 * 1.5
+L = 1.5
 
-offset = - n_side * 2
+half_side_length = (n_side-1) * 0.5 * L
 
+arena_size = half_side_length * 20
+arena_z_center = arena_size * 0.5 - 2
+
+offset = -half_side_length * 7
 drone_locations = generate_random_locations(n_drone,
-                                            offset - half_side_length, -half_side_length,               # origin location
-                                            offset -half_side_length-1, half_side_length+1,            # random x range
-                                            offset -half_side_length-1, half_side_length+1,            # random y range
+                                            offset -half_side_length,   -half_side_length,               # origin location
+                                            offset -half_side_length-1, offset+half_side_length+1,            # random x range
+                                            -half_side_length-1, half_side_length+1,            # random y range
                                             0.5, 1.5,           # near limit and far limit
                                             10000)              # attempt count
 
-drone_xml = generate_drones(drone_locations, 1, 4.2)                 # from label 1 generate drone xml tags, communication range 4.2
+drone_xml = generate_drones(drone_locations, 1, 4.2)                  # from label 1 generate drone xml tags, communication range 4.2
 
-obstacle_xml = generate_3D_rectangular_gate_xml(1,                     # id
-                                                offset + half_side_length + 3, 0, half_side_length + 2, # position
-                                                0, 0, 0, # orientation
-                                                1001,                 # payload
-                                                n_side, 4, 0.1) # size x, knots, thickness
+obstacle_xml = ""
+obstacle_xml += generate_3D_rectangular_gate_xml(1,                    # id
+                                                 -half_side_length*4, 0, half_side_length+2, # position
+                                                 0, 0, 0,              # orientation
+                                                 1001,                 # payload
+                                                 half_side_length*2+3, half_side_length*2+3, 0.2)       # size x, size y, thickness
+
+obstacle_xml += generate_3D_rectangular_gate_xml(2,                    # id
+                                                 half_side_length*2, half_side_length*2+1.5, half_side_length+2, # position
+                                                 0, 0, 0,              # orientation
+                                                 1002,                 # payload
+                                                 half_side_length*2+3, half_side_length*2+3, 0.2)       # size x, size y, thickness
+
+obstacle_xml += generate_3D_rectangular_gate_xml(3,                    # id
+                                                 half_side_length*2, -half_side_length*2-1.5, half_side_length+2, # position
+                                                 0, 0, 0,              # orientation
+                                                 1003,                 # payload
+                                                 half_side_length*2+3, half_side_length*2+3, 0.2)       # size x, size y, thickness
 
 parameters = '''
     mode_2D="false"
@@ -33,6 +55,7 @@ parameters = '''
     drone_tilt_sensor="true"
 
     drone_label="1, 1000"
+    obstacle_label="1001, 1050"
 
     drone_default_start_height="3.0"
     safezone_drone_drone="3"
@@ -45,6 +68,8 @@ parameters = '''
     driver_stop_zone="0.15"
 
     n_drone="{}"
+
+    drone_velocity_mode="true"
 '''.format(n_drone)
 
 # generate argos file
@@ -54,11 +79,13 @@ generate_argos_file("@CMAKE_CURRENT_BINARY_DIR@/simu_code/vns_template.argos",
         ["RANDOMSEED",        str(Inputseed)],  # Inputseed is inherit from createArgosScenario.py
         ["MULTITHREADS",      str(MultiThreads)],  # MultiThreads is inherit from createArgosScenario.py
         ["TOTALLENGTH",       str((Experiment_length or 0)/5)],
+        ["ARENA_SIZE",        str(arena_size)],
+        ["ARENA_Z_CENTER",    str(arena_z_center)],
         ["OBSTACLES",         obstacle_xml],
         ["DRONES",            drone_xml], 
         ["DRONE_CONTROLLER", generate_drone_controller('''
               script="@CMAKE_CURRENT_BINARY_DIR@/simu_code/drone.lua"
-        ''' + parameters)],
+        ''' + parameters, {"velocity_mode":True})],
         ["SIMULATION_SETUP",  generate_physics_media_loop_visualization("@CMAKE_BINARY_DIR@", False)],
     ]
 )
