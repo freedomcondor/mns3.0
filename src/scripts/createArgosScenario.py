f09258e8943edcb4449bf2056ab24706efd67c97
Weixu Zhu <zhuweixu_harry@126.com>
2023-09-26 15:02:32 +0200

    Adjust parameters for drone 3D scenarios

--- src/scripts/createArgosScenario.py
+++ src/scripts/createArgosScenario.py
@@ -89,7 +89,7 @@ def generate_drone_controller(params, option={}) :
         <drone_system implementation="default" />
         <drone_cameras_system implementation="default" show_frustum="{}" show_tag_rays="{}" />
         <drone_flight_system implementation="default" />
-        <drone_rangefinders implementation="default" show_rays="true" />
+        <drone_rangefinders implementation="default" show_rays="false" />
         <radios implementation="default" />
       </sensors>
       <params simulation="true" {} />
@@ -1040,15 +1040,15 @@ def generate3DdroneParameters() :
 
         "safezone_drone_drone" :"15",
 
-        "dangerzone_drone"     :"2.5",
+        "dangerzone_drone"     :"4.0",
         "deadzone_drone"       :"1.5",
-        "dangerzone_aerial_obstacle"  :"2.5",
+        "dangerzone_aerial_obstacle"  :"4.0",
         "deadzone_aerial_obstacle"    :"1.5",
-        "avoid_speed_scalar"   :"5",
+        "avoid_speed_scalar"   :"3",
 
-        "driver_default_speed" :"0.3",
-        "driver_slowdown_zone" :"2.0",
-        "driver_stop_zone"     :"0.50",
+        "driver_default_speed" :"0.5",
+        "driver_slowdown_zone" :"3.0",
+        "driver_stop_zone"     :"0.3",
         "driver_arrive_zone"   :"1.5",
 
         "drone_velocity_mode"  :"true",
