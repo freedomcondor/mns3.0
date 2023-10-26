8661e85fd20c5bfb1e0dab7acc01f8065e0898ab
Weixu Zhu <zhuweixu_harry@126.com>
2023-10-27 00:31:27 +0200

    Upgrade test21, trying to optimize calculation and memory usage

--- src/testing/test_21_scalability/drone.in.lua
+++ src/testing/test_21_scalability/drone.in.lua
@@ -17,24 +17,242 @@ require("morphology")
 local n_drone = tonumber(robot.params.n_drone)
 local structure = generate_morphology(n_drone)
 
-local dataFileName = "record.dat"
+local dataMemLogFileName = "logs_time/mem.dat"
+local dataTimeLogFileName = "logs_time/lua_time_" .. robot.id .. ".dat"
+local dataTimeLogFile
+
+function getCurrentTime()
+	local wallTimeS, wallTimeNS, CPUTimeS, CPUTimeNS = robot.radios.wifi.get_time()
+	return wallTimeS + wallTimeNS * 0.000000001, CPUTimeS + CPUTimeNS * 0.000000001
+end
+
+local lastMeasuredWallTime, lastMeasuredCPUTime
+local currentWallTime, currentCPUTime
+
+-- Overwrite core node to get time
+VNS.create_vns_core_node = function(vns, option)
+	-- option = {
+	--      connector_no_recruit = true or false or nil,
+	--      connector_no_parent_ack = true or false or nil,
+	--      specific_name = "drone1"
+	--      specific_time = 150
+	--          -- If I am stabilizer_preference_robot then ack to only drone1 for 150 steps
+	-- }
+	if option == nil then option = {} end
+	if robot.id == vns.Parameters.stabilizer_preference_robot then
+		option.specific_name = vns.Parameters.stabilizer_preference_brain
+		option.specific_time = vns.Parameters.stabilizer_preference_brain_time
+	end
+
+																local vnsCoreNodeLastMeasuredWallTime, vnsCoreNodeLastMeasuredCPUTime
+	return
+	{type = "sequence", children = {
+		--vns.create_preconnector_node(vns),
+																function()
+																	vnsCoreNodeLastMeasuredWallTime, vnsCoreNodeLastMeasuredCPUTime = getCurrentTime()
+																	return false, true
+																end,
+		vns.Connector.create_connector_node(vns,
+			{	no_recruit = option.connector_no_recruit,
+				no_parent_ack = option.connector_no_parent_ack,
+				specific_name = option.specific_name,
+				specific_time = option.specific_time,
+			}),
+
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.1.____connector_____ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+
+		--vns.DroneConnectKeeper.create_droneconnectkeeper_node(vns),
+		vns.Assigner.create_assigner_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.2.____assigner______ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		vns.ScaleManager.create_scalemanager_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.3.____scalemanager__ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		vns.Stabilizer.create_stabilizer_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.4.____stabilizer____ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		vns.Allocator.create_allocator_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.5.____allocator_____ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		vns.TrussReferencer.create_trussreferencer_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.6.__trussreferencer_ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		vns.IntersectionDetector.create_intersectiondetector_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.7.__intersection____ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		vns.Avoider.create_avoider_node(vns, {
+			drone_pipuck_avoidance = option.drone_pipuck_avoidance
+		}),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.8.__intersection____ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		vns.Spreader.create_spreader_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.9.__spreader________ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		vns.BrainKeeper.create_brainkeeper_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("        3.2.9.__brainkeeper_____ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsCoreNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsCoreNodeLastMeasuredCPUTime))
+																	vnsCoreNodeLastMeasuredWallTime = currentWallTime
+																	vnsCoreNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+		--vns.CollectiveSensor.create_collectivesensor_node(vns),
+		--vns.Driver.create_driver_node(vns),
+	}}
+end
+
+VNS.create_vns_node = function(vns, option)
+	-- option = {
+	--      connector_no_recruit = true or false or nil,
+	--      connector_no_parent_ack = true or false or nil,
+	--      driver_waiting
+	-- }
+	if option == nil then option = {} end
+
+																local vnsNodeLastMeasuredWallTime, vnsNodeLastMeasuredCPUTime
+
+	local children_node = {
+																function()
+																	vnsNodeLastMeasuredWallTime, vnsNodeLastMeasuredCPUTime = getCurrentTime()
+																	return false, true
+																end,
+		vns.create_preconnector_node(vns),
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("    3.1.____preconnector________ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsNodeLastMeasuredCPUTime))
+																	vnsNodeLastMeasuredWallTime = currentWallTime
+																	vnsNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end,
+	}
+	if option.navigation_node_pre_core ~= nil then
+		table.insert(children_node, option.navigation_node_pre_core)
+	end
+	table.insert(children_node, vns.create_vns_core_node(vns, option))
+															table.insert(children_node, 
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("    3.2.____core node___________ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsNodeLastMeasuredCPUTime))
+																	vnsNodeLastMeasuredWallTime = currentWallTime
+																	vnsNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end
+															)
+	if option.navigation_node_post_core ~= nil then
+		table.insert(children_node, option.navigation_node_post_core)
+	end
+	table.insert(children_node,
+		vns.Driver.create_driver_node(vns, {waiting = option.driver_waiting})
+	)
+															table.insert(children_node, 
+																function()
+																	local currentWallTime, currentCPUTime = getCurrentTime()
+																	dataTimeLogFile:write(string.format("    3.3.____driver node_________ %.10f, %.10f\n",
+																	                                    currentWallTime - vnsNodeLastMeasuredWallTime,
+																	                                    currentCPUTime - vnsNodeLastMeasuredCPUTime))
+																	vnsNodeLastMeasuredWallTime = currentWallTime
+																	vnsNodeLastMeasuredCPUTime = currentCPUTime
+																	return false, true
+																end
+															)
+	return {
+		type = "sequence", children = children_node
+	}
+end
 
 function init()
 	api.linkRobotInterface(VNS)
 	api.init() 
+	api.debug.recordSwitch = true
 	vns = VNS.create("drone")
 	reset()
 
 	number = tonumber(string.match(robot.id, "%d+"))
-	if number % 3 == 1 then
-		api.parameters.droneDefaultStartHeight = 1
-	elseif number % 3 == 2 then
-		api.parameters.droneDefaultStartHeight = 3.0
-	elseif number % 3 == 0 then
-		api.parameters.droneDefaultStartHeight = 5.0
+	local baseHeight = 8
+	local distribute_scale = 4
+	if number % 5 == 1 then
+		api.parameters.droneDefaultStartHeight = baseHeight
+	elseif number % 5 == 2 then
+		api.parameters.droneDefaultStartHeight = baseHeight + 1 * distribute_scale
+	elseif number % 5 == 3 then
+		api.parameters.droneDefaultStartHeight = baseHeight + 2 * distribute_scale
+	elseif number % 5 == 4 then
+		api.parameters.droneDefaultStartHeight = baseHeight + 3 * distribute_scale
+	elseif number % 5 == 0 then
+		api.parameters.droneDefaultStartHeight = baseHeight + 4 * distribute_scale
 	end
 
-	--api.debug.show_all = true
+	api.debug.show_all = true
 end
 
 function reset()
@@ -45,61 +263,103 @@ function reset()
 	bt = BT.create(
 		vns.create_vns_node(vns)
 	)
+
+	dataTimeLogFile = io.open(dataTimeLogFileName, "w")
 end
 
 function step()
+	-- Log MEM
 	local getMEM_title_linux  = "top -b -n 1 | grep PID"
 	local getMEM_argos_linux = "top -b -n 1 | grep argos3"
 	local getMEM_title_mac  = "top -l 1 | grep PID"
 	local getMEM_argos_mac = "top -l 1 | grep argos3"
 	local getTotalMEM_linux = "grep MemTotal /proc/meminfo"
 	local getTotalMEM_mac = "sysctl hw.memsize"
-	local getTIME_linux = "date +\"%s.%N\""
-	local getTIME_mac   = "gdate +\"%s.%N\""
 
 	local getMEM_title = getMEM_title_linux
 	local getMEM_argos = getMEM_argos_linux
-	local getTIME      = getTIME_linux
 	local getTotalMEM  = getTotalMEM_linux
 
 	local mac = @CMAKE_APPLE_FLAG@
 	if mac == true then
 		getMEM_title = getMEM_title_mac
 		getMEM_argos = getMEM_argos_mac
-		getTIME      = getTIME_mac
 		getTotalMEM  = getTotalMEM_mac
 	end
 
 	if robot.id == "drone1" then
 		if api.stepCount == 0  then
-			os.execute(getTotalMEM .. " > " .. dataFileName)
-			os.execute(getMEM_title .. " >> " .. dataFileName)
-			os.execute(getTIME .. " >> " .. dataFileName)
+			os.execute(getTotalMEM .. " > " .. dataMemLogFileName)
+			os.execute(getMEM_title .. " >> " .. dataMemLogFileName)
 		end
-		if api.stepCount % 5 == 0  then
-			os.execute("echo ".. tostring(api.stepCount) .. " >> " .. dataFileName)
-			os.execute(getMEM_argos .. " >> " .. dataFileName)
-			os.execute(getTIME .. " >> " .. dataFileName)
+		if api.stepCount % 50 == 0  then
+			os.execute("echo ".. tostring(api.stepCount) .. " >> " .. dataMemLogFileName)
+			os.execute(getMEM_argos .. " >> " .. dataMemLogFileName)
 		end
 	end
 
 	--logger(robot.id, api.stepCount, "----------------------------")
+																	-- log time
+																		lastMeasuredWallTime, lastMeasuredCPUTime = getCurrentTime()
+																		dataTimeLogFile:write("--- " .. tostring(api.stepCount) .. "--------\n")
 	api.preStep()
+																		currentWallTime, currentCPUTime = getCurrentTime()
+																		dataTimeLogFile:write(string.format("1. api_prestep__________________ %.10f, %.10f\n",
+																		                                    currentWallTime - lastMeasuredWallTime,
+																		                                    currentCPUTime - lastMeasuredCPUTime))
+																		lastMeasuredWallTime = currentWallTime
+																		lastMeasuredCPUTime = currentCPUTime
+
 	vns.preStep(vns)
+																		currentWallTime, currentCPUTime = getCurrentTime()
+																		dataTimeLogFile:write(string.format("2. vns_prestep__________________ %.10f, %.10f\n",
+																		                                    currentWallTime - lastMeasuredWallTime,
+																		                                    currentCPUTime - lastMeasuredCPUTime))
+																		lastMeasuredWallTime = currentWallTime
+																		lastMeasuredCPUTime = currentCPUTime
 
 	bt()
 
+																		currentWallTime, currentCPUTime = getCurrentTime()
+																		dataTimeLogFile:write(string.format("3. bt___________________________ %.10f, %.10f\n",
+																		                                    currentWallTime - lastMeasuredWallTime,
+																		                                    currentCPUTime - lastMeasuredCPUTime))
+																		lastMeasuredWallTime = currentWallTime
+																		lastMeasuredCPUTime = currentCPUTime
+
 	vns.postStep(vns)
+																		currentWallTime, currentCPUTime = getCurrentTime()
+																		dataTimeLogFile:write(string.format("4. vns.postStep_________________ %.10f, %.10f\n",
+																		                                    currentWallTime - lastMeasuredWallTime,
+																		                                    currentCPUTime - lastMeasuredCPUTime))
+																		lastMeasuredWallTime = currentWallTime
+																		lastMeasuredCPUTime = currentCPUTime
 	api.postStep()
+																		currentWallTime, currentCPUTime = getCurrentTime()
+																		dataTimeLogFile:write(string.format("5. api.postStep_________________ %.10f, %.10f\n",
+																		                                    currentWallTime - lastMeasuredWallTime,
+																		                                    currentCPUTime - lastMeasuredCPUTime))
+																		lastMeasuredWallTime = currentWallTime
+																		lastMeasuredCPUTime = currentCPUTime
 	--api.debug.showVirtualFrame(true)
 	api.debug.showChildren(vns, {drawOrientation = false})
 	--api.debug.showSeenRobots(vns, {drawOrientation = true})
 	if vns.goal.positionV3:length() < vns.Parameters.driver_stop_zone * 2 then
 		api.debug.showMorphologyLines(vns, true)
 	end
+	vns.logLoopFunctionInfo(vns)
+																		currentWallTime, currentCPUTime = getCurrentTime()
+																		dataTimeLogFile:write(string.format("6. after postStep_______________ %.10f, %.10f\n",
+																		                                    currentWallTime - lastMeasuredWallTime,
+																		                                    currentCPUTime - lastMeasuredCPUTime))
+																		lastMeasuredWallTime = currentWallTime
+																		lastMeasuredCPUTime = currentCPUTime
+
+																		dataTimeLogFile:flush()
 end
 
 function destroy()
 	vns.destroy()
 	api.destroy()
+	io.close(dataTimeLogFile)
 end
--- src/testing/test_21_scalability/run.in.py
+++ src/testing/test_21_scalability/run.in.py
@@ -5,54 +5,41 @@ exec(compile(open(createArgosFileName, "rb").read(), createArgosFileName, 'exec'
 import os
 
 n_drone = Experiment_length                # abuse experiment length for total number
-half_side_length = n_drone ** (1/3) / 2 * 1.5
+half_side_length = n_drone ** (1/3) / 2 * 5
+
+arena_size = half_side_length * 30
+arena_z_center = arena_size / 2 - 2
 
 # drone and pipuck
 drone_locations = generate_random_locations(n_drone,
                                             -half_side_length, -half_side_length,               # origin location
                                             -half_side_length-1, half_side_length+1,            # random x range
                                             -half_side_length-1, half_side_length+1,            # random y range
-                                            0.5, 1.5,           # near limit and far limit
+                                            1.5, 3,           # near limit and far limit
                                             10000)              # attempt count
 
-drone_xml =  generate_drones(drone_locations, 1, 4)                 # from label 1 generate drone xml tags
+drone_xml =  generate_drones(drone_locations, 1, 12)                 # from label 1 generate drone xml tags
 
-parameters = '''
-    mode_2D="false"
-    drone_real_noise="false"
-    drone_tilt_sensor="false"
+parameters = generate3DdroneParameters()
+parameters["n_drone"] = n_drone
 
-    drone_label="1, 1000"
-    obstacle_label="100, 100"
-
-    drone_default_start_height="3.0"
-    safezone_drone_drone="4.0"
-    dangerzone_drone="1"
-
-    report_sight_rounds="2"
-
-    driver_default_speed="0.5"
-    driver_slowdown_zone="1.5"
-    driver_stop_zone="0.15"
-
-    drone_velocity_mode="true"
-
-    n_drone="{}"
-'''.format(n_drone)
+parameters_txt = generateParametersText(parameters)
 
 # generate argos file
 generate_argos_file("@CMAKE_CURRENT_BINARY_DIR@/simu_code/vns_template.argos", 
                     "vns.argos",
     [
         ["RANDOMSEED",        str(Inputseed)],  # Inputseed is inherit from createArgosScenario.py
-        ["TOTALLENGTH",       str(501/5)],
+#        ["TOTALLENGTH",       str(501/5)],
         ["TOTALLENGTH",       str(0)],
         ["DRONES",            drone_xml], 
         #["OBSTACLES",         obstacle_xml], 
+        ["ARENA_SIZE",        str(arena_size)],
+        ["ARENA_Z_CENTER",    str(arena_z_center)],
         ["DRONE_CONTROLLER", generate_drone_controller('''
               script="@CMAKE_CURRENT_BINARY_DIR@/simu_code/drone.lua"
-        ''' + parameters, {"ideal_mode":False, "velocity_mode":True})],
-        ["SIMULATION_SETUP",  generate_physics_media_loop_visualization("@CMAKE_BINARY_DIR@", True)],
+        ''' + parameters_txt, {"ideal_mode":False, "velocity_mode":True})],
+        ["SIMULATION_SETUP",  generate_physics_media_loop_visualization("@CMAKE_BINARY_DIR@", False)],
     ]
 )
 
