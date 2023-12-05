023856c7e188922eeeccb6fae91476e1cd01e76c
Weixu Zhu <zhuweixu_harry@126.com>
2023-12-06 01:29:37 +0800

    Add hollow sphere test in test53

--- src/testing/test_53_truss_3/drone.in.lua
+++ src/testing/test_53_truss_3/drone.in.lua
@@ -17,14 +17,24 @@ local bt
 --require("manGenerator")
 --require("sphere20Generator")
 --require("truckGenerator")
-require("morphologyGenerateSpineSphere")
+--require("morphologyGenerateSpineSphere")
+require("morphologyGenerateHollowSphere")
 
 local n_drone = tonumber(robot.params.n_drone)
 --local structure = create_complex_beam(3, 5, 7)
 --local structure = create_body()
 --local structure = create_sphere20()
 --local structure = create_truck(0)
-local structure = generateSpineCenterBrainSphere(21.5, vector3(), quaternion(), vector3(5.2,0,0), vector3(0, 5.2, 0), vector3(0, 0, -5))
+--local structure = generateSpineCenterBrainSphere(21.5, vector3(), quaternion(), vector3(5.2,0,0), vector3(0, 5.2, 0), vector3(0, 0, -5))
+
+--[[
+local structure = generateFixLengthCircle(10, 5, vector3(), quaternion())
+table.insert(structure.children, generateFixLengthCircle(8, 4, vector3(2,0,7), quaternion()))
+--]]
+
+--local structure = generateCircleLayer(15, 5, math.pi/3 + math.pi/12, vector3(), quaternion())
+local structure = generateFixLengthHollowSphere(15, 4, 5, vector3(), quaternion())
+
 
 function init()
 	api.linkRobotInterface(VNS)
@@ -54,6 +64,7 @@ end
 function reset()
 	vns.reset(vns)
 	if vns.idS == "drone1" then vns.idN = 1 end
+	--if vns.idS == "drone1" then vns.api.virtualFrame.logicOrientationQ = quaternion(math.pi/2, vector3(1,0,0)) end
 	vns.setGene(vns, structure)
 
 	bt = BT.create(
