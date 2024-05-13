6a4fc832f9bb6568043a6256d8a0af870884ec2d
Weixu Zhu <zhuweixu_harry@126.com>
2024-05-13 08:30:40 +0200

    Add builderbot test 05 and test approach and pickup

---
+++ src/builderbot_testing/test_05_approach/builderbot.in.lua
@@ -0,0 +1,54 @@
+if robot.params.simulation == "true" then
+	package.path = package.path .. ";@CMAKE_CURRENT_BINARY_DIR@/simu_code/?.lua"
+	package.path = package.path .. ";@CMAKE_SOURCE_DIR@/core/api/builderbot-utils/?.lua"
+	assert(loadfile(                "@CMAKE_SOURCE_DIR@/core/api/builderbot-utils/init.lua"))()
+else
+	package.path = package.path .. ";/home/root/builderbot-utils/?.lua"
+	assert(loadfile("/home/root/builderbot-utils/init.lua"))()
+end
+
+logger = require("Logger")
+logger.enable()
+robot.logger:enable("nodes_aim_block")
+robot.logger:enable("nodes_curved_approach_block")
+robot.logger:set_verbosity(2)
+
+local BT = require("BehaviorTree")
+local bt
+
+local data = {blocks = {}}
+
+function init()
+	reset()
+	robot.camera_system.enable()
+end
+
+function reset()
+	bt = BT.create
+	{
+		type = "sequence*",
+		children = {
+			robot.nodes.create_approach_block_node(data, function() data.target = {id = 1, offset = vector3(0,0,0)} end, 0.19),
+			robot.nodes.create_pick_up_block_node(data, 0.19),
+			robot.nodes.create_approach_block_node(data, function() data.target = {id = 1, offset = vector3(0,0,1)} end, 0.19),
+			robot.nodes.create_place_block_node(data, 0.19),
+		}
+	}
+end
+
+function step()
+	robot.api.process_blocks(data.blocks)
+
+	logger("--------------------")
+	--logger("tags")
+	--logger(robot.camera_system.tags)
+	--logger("blocks")
+	--logger(data.blocks)
+
+	data.target = {id = #data.blocks}
+	bt()
+end
+
+function destroy()
+	robot.camera_system.disable()
+end
