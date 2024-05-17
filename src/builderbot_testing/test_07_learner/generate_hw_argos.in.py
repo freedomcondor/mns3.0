662dc66c6683f61cd72e4a6ba049482d58ca6274
Weixu Zhu <zhuweixu_harry@126.com>
2024-05-17 15:31:19 +0200

    Add a builderbot testing case for code transfer

---
+++ src/builderbot_testing/test_07_learner/generate_hw_argos.in.py
@@ -0,0 +1,53 @@
+createArgosFileName = "@CMAKE_SOURCE_DIR@/scripts/createArgosScenario.py"
+#execfile(createArgosFileName)
+exec(compile(open(createArgosFileName, "rb").read(), createArgosFileName, 'exec'))
+
+params = '''
+    mode_2D="true"
+    mode_builderbot="true"
+
+    stabilizer_preference_brain="builderbot11"
+
+    pipuck_label="1, 10"
+    builderbot_label="11,11"
+    block_label="255,255"
+
+    connector_waiting_count="15"
+    connector_waiting_parent_count="25"
+    connector_unseen_count="20"
+    connector_heartbeat_count="10"
+'''
+
+drone_params = '''
+    script="drone.lua"
+'''
+
+pipuck_params = '''
+    script="pipuck.lua"
+'''
+
+builderbot_params = '''
+    script="builderbot.lua"
+'''
+
+# generate argos file
+generate_argos_file("@CMAKE_SOURCE_DIR@/scripts/argos_templates/drone_hw.argos",
+                    "@CMAKE_CURRENT_BINARY_DIR@/hw_code/01_hw_drone.argos",
+	[
+		["PARAMS",       "driver_default_speed=\"0.03\"\n" + drone_params + params],
+	]
+)
+
+generate_argos_file("@CMAKE_SOURCE_DIR@/scripts/argos_templates/pipuck_hw.argos",
+                    "@CMAKE_CURRENT_BINARY_DIR@/hw_code/02_hw_pipuck.argos",
+	[
+		["PARAMS",       pipuck_params + params],
+	]
+)
+
+generate_argos_file("@CMAKE_SOURCE_DIR@/scripts/argos_templates/builderbot_hw.argos",
+                    "@CMAKE_CURRENT_BINARY_DIR@/hw_code/03_hw_builderbot.argos",
+	[
+		["PARAMS",       builderbot_params + params],
+	]
+)
---
+++ src/builderbot_testing/test_07_learner/pipuck.in.lua
@@ -0,0 +1,83 @@
+if robot.params.simulation == "true" then
+	package.path = package.path .. ";@CMAKE_CURRENT_BINARY_DIR@/simu_code/?.lua"
+end
+
+logger = require("Logger")
+logger.register("main")
+pairs = require("AlphaPairs")
+local api = require("pipuckAPI")
+local VNS = require("VNS")
+local BT = require("DynamicBehaviorTree")
+
+-- datas ----------------
+local bt
+--local vns  -- global vns to make vns appear in lua_editor
+
+local structure = require("morphology")
+
+function init()
+	api.linkRobotInterface(VNS)
+	api.init() 
+	vns = VNS.create("pipuck")
+	reset()
+
+--	api.debug.show_all = true
+end
+
+function reset()
+	vns.reset(vns)
+	if vns.idS == robot.params.stabilizer_preference_brain then vns.idN = 1 end
+	vns.setGene(vns, structure)
+	--[[
+	bt = BT.create
+		{type = "sequence", children = {
+			vns.create_preconnector_node(vns),
+		}}
+	--]]
+	bt = BT.create(vns.create_vns_node(vns, {
+		navigation_node_post_core = {type = "sequence", children = {
+			vns.Learner.create_learner_node(vns),
+			--vns.Learner.create_knowledge_node(vns, "print_test"),
+		}}
+	}))
+
+	if robot.id == "pipuck2" then
+		vns.learner.knowledges["move_forward"] = {hash = 1, rank = 1, node = [[
+			function()
+				vns.Spreader.emergency_after_core(vns, vector3(0.05,0,0), vector3())
+				return false, true
+			end
+		]]}
+	end
+end
+
+function step()
+	logger(robot.id, api.stepCount, "----------------------------")
+	api.preStep()
+	vns.preStep(vns)
+
+	bt()
+
+	if robot.id == "pipuck2" then
+		vns.Learner.spreadKnowledge(vns, "move_forward", vns.learner.knowledges["move_forward"])
+	end
+
+	vns.postStep(vns)
+	api.postStep()
+	api.debug.showVirtualFrame()
+	api.debug.showChildren(vns, {drawOrientation = false})
+	--api.debug.showSeenRobots(vns, {drawOrientation = true})
+	logger("seenRobots")
+	logger(vns.connector.seenRobots)
+	logger("connector")
+	logger(vns.connector)
+	logger("parent")
+	logger(vns.parentR)
+	logger("children")
+	logger(vns.childrenRT)
+end
+
+function destroy()
+	vns.destroy()
+	api.destroy()
+end
