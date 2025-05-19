6532100693a6552bfc48da469ad9b18707319fa6
Weixu Zhu <zhuweixu_harry@126.com>
2025-05-19 20:20:43 +0800

    Tweak exp23 plot

--- src/experiments/exp_23_builderbot_scenario_3_large_simulation/scripts/drawLines_plot.in.py
+++ src/experiments/exp_23_builderbot_scenario_3_large_simulation/scripts/drawLines_plot.in.py
@@ -9,42 +9,36 @@ import os
 #ExperimentsDIR = "@CMAKE_MNS_DATA_PATH@/exp_23_builderbot_scenario_3_large_simulation_backup"
 ExperimentsDIR = "@CMAKE_MNS_DATA_PATH@/exp_23_builderbot_scenario_3_large_simulation"
 
-fig = plt.figure(figsize=(10, 8))  # Make figure taller for four subplots
+fig = plt.figure(figsize=(10, 4))  # Make figure taller for four subplots
 
 DATADIR = ExperimentsDIR + "/run_data"
 
+def readAndDraw(filename, ax, color, step_time_scalar=5):
+    # read data sets
+    dataSet = []
+    for subfolder in getSubfolders(DATADIR):
+        dataSet.append(readDataFrom(subfolder + filename))
+    
+    # process and draw data
+    stepsData, X = transferTimeDataToRunSetData(dataSet)
+    mean, mini, maxi, upper, lower = calcMeanFromStepsData(stepsData)
+    X = [i / step_time_scalar for i in X]
+    drawShadedLinesInSubplot(X, mean, maxi, mini, upper, lower, ax, {'color':color})
+
 # First subplot - original data
-ax1 = fig.add_subplot(1, 1, 1)
-ax2 = ax1.twinx()
+ax1 = fig.add_subplot(2, 1, 1)
+ax2 = fig.add_subplot(2, 1, 2)
 
-# read data sets for first subplot
-dataSet1 = []
-dataSet2 = []
-dataSet3 = []
-for subfolder in getSubfolders(DATADIR):
-    dataSet1.append(readDataFrom(subfolder + "result_helping_size.dat"))
-    dataSet2.append(readDataFrom(subfolder + "result_move_left_size.dat"))
-    dataSet3.append(readDataFrom(subfolder + "result_forward_2_size.dat"))
-
-step_time_scalar = 5
-#----- data1 -----
-stepsData1, X1 = transferTimeDataToRunSetData(dataSet1)
-mean1, mini1, maxi1, upper1, lower1 = calcMeanFromStepsData(stepsData1)
-X1 = [i / step_time_scalar for i in X1]
-drawShadedLinesInSubplot(X1, mean1, maxi1, mini1, upper1, lower1, ax1, {'color':'blue'})
-
-#----- data2 -----
-stepsData2, X2 = transferTimeDataToRunSetData(dataSet2)
-mean2, mini2, maxi2, upper2, lower2 = calcMeanFromStepsData(stepsData2)
-X2 = [i / step_time_scalar for i in X2]
-drawShadedLinesInSubplot(X2, mean2, maxi2, mini2, upper2, lower2, ax1, {'color':'red'})
-
-#----- data3 -----
-stepsData3, X3 = transferTimeDataToRunSetData(dataSet3)
-mean3, mini3, maxi3, upper3, lower3 = calcMeanFromStepsData(stepsData3)
-X3 = [i / step_time_scalar for i in X3]
-drawShadedLinesInSubplot(X3, mean3, maxi3, mini3, upper3, lower3, ax1, {'color':'green'})
+# read and draw all datasets
+readAndDraw("result_forward_size.dat", ax1, 'yellow')
+readAndDraw("result_meet_obstacle_size.dat", ax1, 'orange')
+readAndDraw("result_send_help_size.dat", ax1, 'green')
+readAndDraw("result_wait_to_help_size.dat", ax1, 'red')
+readAndDraw("result_helping_size.dat", ax1, 'blue')
+readAndDraw("result_move_left_size.dat", ax1, 'red')
+readAndDraw("result_forward_2_size.dat", ax1, 'green')
+readAndDraw("learner_length.dat", ax2, 'blue')
 
 plt.tight_layout()
-plt.show()
-#plt.savefig("exp22_plot_combined.pdf")
+#plt.show()
+plt.savefig("exp23_plot_combined.pdf")
